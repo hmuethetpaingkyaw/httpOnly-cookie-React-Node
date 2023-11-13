25fd61d525eca179abc49ad32ff355d0cff0b8cc
Hmue Thet Paing Kyaw <94979276+hmuethetpaingkyaw@users.noreply.github.com>
2023-11-13 22:55:08 +0630

    httponly middleware, jwt token

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,106 @@
-import React from 'react';
-import './App.css'
-import axios from 'axios'
-axios.defaults.withCredentials = true
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 function App() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
+  const [user, setCurrentUser] = useState(null);
 
-const createCookie = () => {
-  axios.get('http://localhost:4000',{ withCredentials: true }).then((res) =>{
-    console.log(res.data)
-  })
-}
-const deleteCookie = () => {
-  axios.get('http://localhost:4000/deleteCookie',{ withCredentials: true }).then((res) =>{
-    console.log(res.data)
-  })
-}
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
 
+  const getCurrentUser = () => {
+    axios
+      .get("http://localhost:4000/current-user", { withCredentials: true })
+      .then((res) => {
+        setCurrentUser(res.data);
+      })
+      .catch((error) => {
+        // setError(error?.reponse?.data?.error);
 
-	return (
-		<div className="App">
-      <h1>HTTP ONLY COOKIE DEMO</h1>
-			<div className="box">
-        <button className="button green" onClick={createCookie}>Create Cookies</button>
-        <button className="button yellow">Renew Cookies</button>
-        <button className="button red" onClick={deleteCookie}>Delete Cookie</button>
+        console.log(error);
+      });
+  };
+
+  const login = async () => {
+    await axios
+      .get("http://localhost:4000/login", { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        // setError(error?.reponse?.data?.error);
+
+        console.log(error);
+      });
+  };
+  const getProducts = () => {
+    if (!localStorage.getItem("httpOnly")) {
+      return;
+    }
+    axios
+      .get("http://localhost:4000/products", { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        // setError(error?.reponse?.data?.error);
+
+        if (error?.response?.data?.status === 401) {
+          localStorage.removeItem("httpOnly");
+        }
+      });
+  };
+
+  const logout = () => {
+    axios
+      .get("http://localhost:4000/logout", { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+        localStorage.removeItem("httpOnly");
+      })
+      .catch((error) => {
+        //setError(error?.response?.data?.error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <h1>{user?.name}</h1>
+      <div
+        style={{
+          background: "white",
+        }}
+      >
+        <span
+          style={{
+            color: "red",
+          }}
+        >
+          {error}
+        </span>
       </div>
-		</div>
-	);
+      <div className="box">
+        {user ? (
+          <>
+            <button className="button yellow" onClick={getProducts}>
+              Get Items
+            </button>
+            <button className="button red" onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <button className="button green" onClick={login}>
+            Login
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,23 +1,91 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const cors = require('cors');
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+const cors = require("cors");
+const { serialize } = require("cookie");
+const jwt = require("jsonwebtoken");
+const { jwtDecode } = require("jwt-decode");
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.get('/', (req, res) => {
-	res
-		.status(202)
-		.cookie('Name', 'Rahul Ahire', {
-			sameSite: 'strict',
-			path: '/',
-			expires: new Date(new Date().getTime() + 100 * 1000),
-            httpOnly: true,
-		}).send("cookie being initialised")
+
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
-app.get('/deleteCookie', (req, res) => {
-	res
-		.status(202)
-		.clearCookie('Name').send("cookies cleared")
+app.get("/login", (req, res) => {
+  jwt.sign(
+    {
+      name: "hmue",
+    },
+    "hmue_httpOnly",
+    {
+      expiresIn: 60 * 60 * 24 * 30,
+    },
+    (_err, token) => {
+      const serialized = serialize("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+      res.setHeader("Set-Cookie", serialized);
+      res.status(200).send({ success: true });
+    }
+  );
 });
+app.get("/logout", (req, res) => {
+  const { cookies } = req;
+
+  const jwt = cookies.token;
+
+  if (!jwt) {
+    return res.status(401).json({
+      status: "error",
+      error: "Unauthorized",
+    });
+  }
+
+  res.status(202).clearCookie("token").send("cookies cleared");
+});
+app.get("/products", (req, res) => {
+  const { cookies } = req;
+
+  const jwt = cookies.token;
+
+  if (!jwt) {
+    return res.status(401).json({
+      status: "error",
+      error: "Unauthorized",
+    });
+  }
+
+  res.status(200).send({
+    data: [
+      {
+        name: "Product 1",
+      },
+      {
+        name: "Product 2",
+      },
+    ],
+  });
+});
+
+app.get("/current-user", (req, res) => {
+	const { cookies } = req;
+  
+	const jwt = cookies.token;
+  
+	if (!jwt) {
+	  return res.status(401).json({
+		status: "error",
+		error: "Unauthorized",
+	  });
+	}
+  
+	res.status(202).send(jwtDecode(jwt));
+  });
+
 app.listen(4000);
